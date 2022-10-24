2913dbab7145d00990564504d7a9a07267f0df36
hugoRGB <huguerrabarajas@gmail.com>
2022-10-24 14:27:44 -0500

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer fixed v-model="drawer" app>
+      <v-list dense>
+        <router-link v-bind:to="{ name: 'inicio' }" class="side_bar_link">
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>home</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>Incio</v-list-item-content>
+          </v-list-item>
+        </router-link>
+        <router-link v-bind:to="{name: 'contacto'}" class="side_bar_link">
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>contact_mail</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>Contacto</v-list-item-content>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar color="indigo" dark fixed app>
+      <v-toolbar-nav-icon @click.stop="drawer = !drawer"></v-toolbar-nav-icon>
+      <v-toolbar-title>Inicio</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <div id="app">
+          <router-view/>
+        </div>
+      </v-container>
+    </v-main>
+    <v-footer color="indigo" app>
+      <span class="white--text">FullStack1 &copy; 2022</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import './assets/stylesheets/main.css';
+
+export default {
+  data: () => ({
+    drawer: null,
+  }),
+  props: {
+    source: String,
+  },
+};
+</script>
---
+++ src/components/Inicio.vue
@@ -0,0 +1,170 @@
+<template>
+  <v-layout row wrap>
+      <v-flex xs4>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Batman vs Superman</div>
+                <span class="grey--text">2016 &middot; Ciencia Ficci&oacute;n/
+                  Acci&oacute;n &middot; 3h 3m</span>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            Han pasado casi dos años desde que la colosal
+            batalla de Superman (Henry Cavill) con Zod (Michael Shannon)
+            devastó la ciudad de Metrópolis. La pérdida de vidas y los
+            daños colaterales hicieron que muchos se sintieran
+            enojados e impotentes, incluido el multimillonario que
+            lucha contra el crimen Bruce Wayne (Ben Affleck).
+            Convencido de que Superman es ahora una amenaza para
+            la humanidad, Batman se embarca en una vendetta personal
+            para poner fin a su reinado en la Tierra, mientras que el
+            intrigante Lex Luthor (Jesse Eisenberg) lanza su
+            propia cruzada contra el Hombre de Acero.
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="purple">Calificar Pel&iacute;cula</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Logan</div>
+                <span class="grey--text">2017 &middot; Ciencia Ficci&oacute;n/
+                  Drama &middot; 2h 21m</span>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            Han pasado los años y los mutantes están en declive. Logan,
+            débil y cansado, vive alejado de todos hasta que acepta una última
+            misión de Charles Xavier: la de proteger a una joven especial,
+            de nombre Laura Kinney pero conocida como X-23, la última esperanza
+            de la raza mutante.
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="purple">Calificar Pel&iacute;cula</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Spider-man: No Way Home</div>
+                <span class="grey--text">2021 &middot; Ciencia Ficci&oacute;n/
+                  Acci&oacute;n &middot; 2h 46m</span>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            Por primera vez en la historia cinematográfica de Spider-Man, nuestro
+            héroe, vecino y amigo es desenmascarado, y por tanto, ya no es capaz de
+            separar su vida normal de los enormes riesgos que conlleva ser un superhéroe.
+            Cuando pide ayuda al Doctor Strange, los riesgos pasan a ser aún más peligrosos,
+            obligándole a descubrir lo que realmente significa ser él.
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="purple">Calificar Pel&iacute;cula</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Forrest Gump</div>
+                <span class="grey--text">1994 &middot; Drama/
+                  Romance &middot; 2h 22m</span>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            Sentado en un banco en Savannah, Georgia, Forrest Gump espera al autobús.
+            Mientras éste tarda en llegar, el joven cuenta su vida a las personas que
+            se sientan a esperar con él. Aunque sufre un pequeño retraso mental,
+            esto no le impide hacer cosas maravillosas. Sin entender del todo lo que
+            sucede a su alrededor, Forrest toma partido en los eventos más importantes
+            de la historia de los Estados Unidos.
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="purple">Calificar Pel&iacute;cula</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Fate Stay Night: Heaven's Feel I. Presage Flower</div>
+                <span class="grey--text">2017 &middot; Fantas&iacute;a
+                  /Acci&oacute;n &middot; 2h</span>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            El Santo Grial, un artefacto que puede cumplir cualquier deseo,
+            es otorgado al ganador de una batalla conocida como la Guerra del Santo Grial.
+            En esta feroz batalla los Maestros (Magos) y Sirvientes (Héroes de la mitología
+            y leyendas) pelearán a muerte. La anterior guerra destruyó una ciudad y a todos
+            sus habitantes. 10 años después la nueva Guerra por el Santo Grial está por comenzar.
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="purple">Calificar Pel&iacute;cula</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Bohemian Rhapsody: la historia de Freddie Mercury</div>
+                <span class="grey--text">2018 &middot; Musical/
+                  Drama &middot; 2h 14m</span>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            Londres, 1970. El joven Farrokh, Freddie para los amigos, trabaja en el aeropuerto,
+            estudia diseño gráfico y escribe canciones. Un día, tras escuchar al grupo Smile en
+            un bar, se ofrece como cantante de la banda al enterarse de que se han quedado sin
+            vocalista. Ninguno de ellos se imaginaba por aquel entonces que se convertirían en
+            los mismísimos Queen.
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="purple">Calificar Pel&iacute;cula</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">My Hero Academia: World Heroes Mission</div>
+                <span class="grey--text">2021 &middot; Acci&oacute;n/
+                  Aventura &middot; 1h 45m</span>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            Deku es perseguido por la policía, acusado de asesinato en masa,
+            mientras que el resto de super héroes han de enfrentarse a un incidente que afecta
+            a todo el mundo. Tercera película de la serie anime My Hero Academia.
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="purple">Calificar Pel&iacute;cula</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+  </v-layout>
+</template>
