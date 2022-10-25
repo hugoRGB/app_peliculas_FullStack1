82da1b0f150936e173565719b69af8f3c1d7e62a
hugoRGB <huguerrabarajas@gmail.com>
2022-10-25 16:18:28 -0500

    Clase 25/10/22

--- src/components/AgregarPelicula.vue
+++ src/components/AgregarPelicula.vue
@@ -1,5 +1,52 @@
 <template>
-    <v-layout>
-        Esto es AgregarPelicula
-    </v-layout>
+    <v-form v-model="validado" ref="form" lazy-validation>
+        <v-text-field label="Nombre Película:" v-model="nombre" :rules="reglasNombre" required>
+        </v-text-field>
+        <v-text-field label="Descripción:" v-model="descripcion" multi-line></v-text-field>
+        <v-select label="Año de publicación:" v-model="anho_pub" :items="anhos"></v-select>
+        <v-text-field label="Género:" v-model="genero"></v-text-field>
+        <v-btn @click="enviar" :disabled="!validado">Enviar</v-btn>
+        <v-btn @click="limpiar">Limpiar</v-btn>
+    </v-form>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    validado: true,
+    nombre: '',
+    descripcion: '',
+    genero: '',
+    anho_pub: '',
+    reglasNombre: [
+      v => !!v || 'Se requiere el nombre de la película.',
+    ],
+    select: null,
+    anhos: [
+      '2022',
+      '2021',
+      '2020',
+      '2019',
+      '2018',
+      '2016',
+      '2015',
+      '2014',
+      '2013',
+      '2012',
+      '2011',
+      '2010',
+    ],
+  }
+  ),
+  methods: {
+    enviar() {
+      if (this.$refs.form.validate()) {
+        // Realizamos una acción
+      }
+    },
+    limpiar() {
+      this.$refs.form.reset();
+    },
+  },
+};
+</script>
